b6f404c470a5e8213c89288c6619da1c158fd803
Dastrilla <dastrilla@yandex.ru>
2024-09-09 14:58:37 +1100

    create index views functions, add templates and static, create urls patterns

--- sakhhub/posts/views.py
+++ sakhhub/posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Post
 # Create your views here.
+
+def index(request):
+    latest = Post.objects.order_by("-pub_date")[:10]
+    context = {"posts":latest}
+
+    return render(request, 'index.html', context)

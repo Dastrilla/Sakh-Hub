eb867406855769d9909799346cdca8945c264256
Dastrilla <dastrilla@yandex.ru>
2024-09-10 15:53:26 +1100

    create form and model for registration, create templates file and etc

--- sakhhub/sakhhub/urls.py
+++ sakhhub/sakhhub/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include('posts.urls')),
+    path("", include("posts.urls")),
+    path("auth/", include("users.urls")),
+    path("auth/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
+
 ]
 
